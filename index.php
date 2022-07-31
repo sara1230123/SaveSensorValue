b7be457660f65e23ccad43d9e9c2e78fa82bc189
sara1230123 <109726481+sara1230123@users.noreply.github.com>
2022-07-31 16:07:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>ٍStore Data from Sensor</title>
+        <style></style>
+        
+<link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+
+        <top>
+            <h1>Store Sensor Value in Database </h1>
+            <hr/>
+            <form action="display.php" method="GET">
+            enter valuse of sensor <input type="number" name="value"></br>
+            <input type="submit" value="submit" name="submit">
+
+            
+            </form>
+        </top>
+        
+    </body>
+</html>  
